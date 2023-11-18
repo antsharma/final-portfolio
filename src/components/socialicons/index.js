0d4c2cb83c7485e8c0ad3ea72ad9ac06a8ab92ff
Anant <antsharma02@gmail.com>
2023-11-19 00:39:03 +0530

    Main framewrk done

---
+++ src/components/socialicons/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./style.css";
+import {
+  FaGithub,
+  FaTwitter,
+  FaFacebookF,
+  FaLinkedin,
+  FaYoutube,
+  FaTwitch,
+} from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export const Socialicons = (params) => {
+  return (
+    <div className="stick_follow_icon">
+      <ul>
+        {/* {socialprofils.twitter && (
+          <li>
+            <a href={socialprofils.twitter}>
+              <FaTwitter />
+            </a>
+          </li>
+        )} */}
+        {socialprofils.github && (
+          <li>
+            {/* <a href={socialprofils.github}> */}
+            <a href="https://github.com/antsharma">
+              <FaGithub />
+            </a>
+          </li>
+        )}
+        {socialprofils.facebook && (
+          <li>
+            <a href="https://www.facebook.com/profile.php?id=100009739217528">
+              <FaFacebookF />
+            </a>
+          </li>
+        )}
+        {socialprofils.linkedin && (
+          <li>
+            {/* <a href={socialprofils.linkedin}> */}
+            <a href="https://www.linkedin.com/in/anant-sharma23/">
+              <FaLinkedin />
+            </a>
+          </li>
+        )}
+        {/* {socialprofils.youtube && (
+          <li>
+            <a href={socialprofils.youtube}>
+              <FaYoutube />
+            </a>
+          </li>
+        )}
+        {socialprofils.twitch && (
+          <li>
+            <a href={socialprofils.twitch}>
+              <FaTwitch />
+            </a>
+          </li>
+        )} */}
+      </ul>
+      <p>Follow Me</p>
+    </div>
+  );
+};
---
+++ src/content_option.js
@@ -0,0 +1,182 @@
+const logotext = "ANANT";
+const meta = {
+    title: "Anant Sharma",
+    description: "I’m Anant Sharma, pre-final year at IIIT Allahabad, Full stack devloper uses MERN and gets inspired from Anime",
+};
+
+const introdata = {
+    title: "I’m Anant Sharma",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I binge watch Anime",
+    },
+    //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "I'm Anant, a pre-final year student at IIITA, pursuing a degree in Information Technology. I am a quick learner with a strong foundation of skills developed through my studies and various projects. I have a deep passion for software engineering and a genuine excitement for building websites. I'am an avid reader delving into mystery, thrillers and romance. To maintain the balance of life, I dabble in Anime and football from time to time. ",
+};
+const worktimeline = [{
+        jobtitle: "Fresher",
+        where: "IIITA",
+        date: "2023-present",
+    },
+    // {
+    //     jobtitle: "Head Of Public Relations",
+    //     where: "Effervescence-Cultural Fest of IIITA",
+    //     date: "2023-present",
+    // },
+    // {
+    //     jobtitle: "Member: Robotics Wing",
+    //     where: "Tesla- Electronics Society",
+    //     date: "2022-2023",
+    // },
+];
+
+const skills = [{
+        name: "C/C++",
+        value: 90,
+    },
+    {
+        name: "JAVA",
+        value: 50,
+    },
+    {
+        name: "MySQL",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "MongoDB",
+        value: 60,
+    },
+    {
+        name: "Express",
+        value: 60,
+    },
+    {
+        name: "NodeJS",
+        value: 50,
+    },
+    {
+        name: "Firebase",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "Overall Coordinator- Nirmiti: The Arts Society",
+        description: "Responsible for managing the budget, planning and organising various art events throughout the year.",
+    },
+    {
+        title: "Head of Public Relations- Effervescence: Cultural Fest of IIITA",
+        description: "Responsible for participation, maintaining mutually beneficial relationships with other institutions and inviting various guests and artists to the event.",
+    },
+    {
+        title: "Member- Robotics Wing, TESLA",
+        description: "Organised events and workshops for students in the robotics domain and worked with CIR Lab IIITA",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Library management system using JavaScript, MySQL and NodeJs ",
+        link: "https://github.com/antsharma/Library-Management-System",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Flipkart GRID 5.0 Semi finalists",
+        link: "https://github.com/antsharma/hackout-23",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Social-chitchat application using ReactJs and firebase",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Chat application using socket programming and AES encryption along with Diffie-Hellman Key exchange",
+        link: "https://github.com/antsharma/Network-security-AES-chat-app",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Social Media Clone",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
